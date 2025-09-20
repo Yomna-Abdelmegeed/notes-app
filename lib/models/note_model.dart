551d0677b2ead9752fa2cb0c32c670d6ee535aeb
Yomna Abdelmegeed <yomna.tota4@gmail.com>
2025-09-20 23:54:40 +0300

    Create notes model.

---
+++ lib/models/note_model.dart
@@ -0,0 +1,11 @@
+class NoteModel {
+  final String title;
+  final String subtitle;
+  final String date;
+
+  NoteModel({
+    required this.title,
+    required this.subtitle,
+    required this.date,
+  });
+}
