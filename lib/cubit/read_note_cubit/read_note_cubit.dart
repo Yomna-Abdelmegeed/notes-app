cd7ae6a4e9373f0f06bcc7c621371ce681349079
Yomna Abdelmegeed <yomna.tota4@gmail.com>
2025-09-24 02:49:25 +0300

    Create read note cubit.

---
+++ lib/cubit/read_note_cubit/read_note_cubit.dart
@@ -0,0 +1,18 @@
+import 'package:bloc/bloc.dart';
+import 'package:hive/hive.dart';
+import 'package:meta/meta.dart';
+import 'package:notes_app/constants.dart';
+import 'package:notes_app/models/note_model.dart';
+
+part 'read_note_state.dart';
+
+class ReadNoteCubit extends Cubit<ReadNoteState> {
+  ReadNoteCubit() : super(ReadNoteInitial());
+
+  List<NoteModel>? notes;
+
+  readNote() {
+    var noteBox = Hive.box<NoteModel>(kNotesBox);
+    notes = noteBox.values.toList();
+  }
+}
