b55889b0de86dc8fe6f3737e4aa17b099767472e
Yomna Abdelmegeed <yomna.tota4@gmail.com>
2025-09-25 22:39:02 +0300

    format the date.

--- lib/simple_bloc_observer.dart
+++ lib/simple_bloc_observer.dart
@@ -4,7 +4,7 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 class SimpleBlocObserver implements BlocObserver {
   @override
   void onChange(BlocBase bloc, Change change) {
-    print("Change = $change");
+    debugPrint("Change = $change");
   }
 
   @override
@@ -19,16 +19,13 @@ class SimpleBlocObserver implements BlocObserver {
 
   @override
   void onError(BlocBase bloc, Object error, StackTrace stackTrace) {
-    // TODO: implement onError
   }
 
   @override
   void onEvent(Bloc bloc, Object? event) {
-    // TODO: implement onEvent
   }
 
   @override
   void onTransition(Bloc bloc, Transition transition) {
-    // TODO: implement onTransition
   }
 }
--- lib/view/widgets/notes_form.dart
+++ lib/view/widgets/notes_form.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:intl/intl.dart';
 import 'package:notes_app/cubit/add_note_cubit/add_note_cubit.dart';
 import 'package:notes_app/models/note_model.dart';
 import 'package:notes_app/view/widgets/custom_bottom.dart';
@@ -52,10 +53,12 @@ class _NotesFormState extends State<NotesForm> {
                 onTap: () {
                   if (formKey.currentState!.validate()) {
                     formKey.currentState!.save();
+                    DateTime now = DateTime.now();
+                    String formattedDate = DateFormat.yMMMMd().format(now);
                     NoteModel note = NoteModel(
                       title: title!,
                       subtitle: subTitle!,
-                      date: DateTime.now().toString(),
+                      date: formattedDate,
                     );
                     BlocProvider.of<AddNoteCubit>(context).addNote(note);
                   } else {
